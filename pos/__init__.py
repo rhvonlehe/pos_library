46e455102851e5035f3663d89febb10e60c45d41
Richard von Lehe <rhvonlehe@gmail.com>
2015-10-24 15:11:42 -0500

    updates

--- pos/__init__.py
+++ pos/__init__.py
@@ -5,8 +5,10 @@ omnivore API wrapper
 """
 
 import requests
+#import json # TODO - remove
 
 
+# TODO - remove
 #
 #class Config:
 #   """A class containing the configuration of the omnivore site"""
@@ -15,7 +17,7 @@ import requests
 
 class Pos:
 
-    # TODO: possibly move some of this to Config class
+    # TODO: possibly move some of this to separate Config class
     #
     _version = '0.1'
     _location_base = 'https://api.omnivore.io/' + _version + '/locations/'
@@ -28,17 +30,36 @@ class Pos:
 
     def __init__(self, api_key, location):
         self._api_key = api_key
-        self._location = self._location_base + location + '/'
+        self._url_base = self._location_base + location + '/'
         self._headers={'Api-Key': self._api_key}
 
-    def _get_uri(self, resource):
-        return self._location + self.API_PATHS[resource]
+    def _get_url(self, resource):
+        return self._url_base + self.API_PATHS[resource]
 
+    def _get_json_content(self, resource):
+        req = self._get_url(resource)
+        resp = requests.get(req, headers=self._headers)
+
+        return resp.json()
 
     def employees(self):
-        req = self._get_uri('employees')
-        print req
-        r = requests.get(req, headers=self._headers)
-        #        print r.json()
+        # req = self._get_url('employees')
+        # resp = requests.get(req, headers=self._headers)
+        #
+        # resp = resp.json()
 
-        return r.text
+        resp = self._get_json_content('employees')
+
+        print resp
+        # embedded = json['_embedded']
+        # print embedded
+        #print embedded['employees']
+        # for emp in embedded['_employees']:
+        #     print emp
+
+
+        # print json['count']
+        # print json['_links']
+        # print json['_embedded']
+
+        return #r.text
