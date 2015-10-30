c354533fe58ba7f416b9c932e49771b473ccdfd6
unknown <C9917732@USPLYWHD6TKQ1.eaton.ad.etn.com>
2015-10-30 11:08:36 -0500

    Remove old code and unneeded comments

--- pos/__init__.py
+++ pos/__init__.py
@@ -10,15 +10,6 @@ from revenue_center import RevenueCenter
 from order_type import OrderType
 from table import Table
 
-#import requests # TODO - remove
-
-
-# TODO - remove
-#
-#class Config:
-#   """A class containing the configuration of the omnivore site"""
-#
-
 
 class Pos:
 
@@ -26,13 +17,6 @@ class Pos:
     #
     _version = '0.1'
     _location_base = 'https://api.omnivore.io/' + _version + '/locations/'
-
-    # TODO - remove
-    # API_PATHS = {'employees': 'employees',
-    #              'revenue_centers': 'revenue_centers',
-    #              'tables': 'tables',
-    #              'order_types': 'order_types',
-    #              'tickets': 'tickets'}
 
     def __init__(self, api_key, location):
         self._api_key = api_key
@@ -112,44 +96,3 @@ class Pos:
 
 
 
-        # TODO - remove below
-        #
-        # print
-        # print emps()
-        # print emps['employees'][0].state
-        # print emps['employees'][0].state['first_name']
-        # print
-        #
-
-
-    # TODO - remove below
-    # def employees(self):
-    #     # req = self._get_url('employees')
-    #     # resp = requests.get(req, headers=self._headers)
-    #     #
-    #     # resp = resp.json()
-    #
-    #     resp = self._get_json_content('employees')
-    #
-    #     print resp
-    #     # embedded = json['_embedded']
-    #     # print embedded
-    #     #print embedded['employees']
-    #     # for emp in embedded['_employees']:
-    #     #     print emp
-    #
-    #
-    #     # print json['count']
-    #     # print json['_links']
-    #     # print json['_embedded']
-    #
-    #     return #r.text
-
-    # def _get_url(self, resource):
-    #     return self._url_base + self.API_PATHS[resource]
-    #
-    # def _get_json_content(self, resource):
-    #     req = self._get_url(resource)
-    #     resp = requests.get(req, headers=self._headers)
-    #
-    #     return resp.json()
